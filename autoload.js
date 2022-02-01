6163814a3963fa322f4a257b20cb5817a6626c06
wuqirui <53338059+hhdqirui@users.noreply.github.com>
2022-02-02 07:30:51 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -34,7 +34,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/hhdqirui/live2d_api/"
+			cdnPath: "https://cdn.jsdelivr.net/gh/hhdqirui/live2d_api@0.2/"
 		});
 	});
 }
